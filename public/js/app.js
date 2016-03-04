47e586a3150408f2f2a242dc350360a177de62a1
Gaurav <gauravagerwala@gmail.com>
2016-03-04 21:33:06 +0530

    Showing message on browser on message event

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,8 @@ socket.on('connect', function(){
 
 socket.on('message', function(message){
 	console.log('New Message : ' + message.text);
+
+	jQuery('.messages').append('<p>' + message.text + '</p>');
 });
 
 //Handles submitting new message
@@ -14,7 +16,7 @@ var $form = jQuery('#message-form');
 $form.on('submit', function(event){
 	event.preventDefault();
 
-	var $message = find('input[name=message')''
+	var $message = $form.find('input[name=message]');
 
 	socket.emit('message', {
 		text: $message.val()
